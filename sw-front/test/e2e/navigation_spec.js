6f9641979c9a85a4c3ddda151285aeb0f878474c
Nick Bonnet <nbonnet@follett.com>
2014-04-11 15:00:59 -0500

    stuff

--- sw-front/test/e2e/navigation_spec.js
+++ sw-front/test/e2e/navigation_spec.js
@@ -2,10 +2,18 @@
 
 describe('Navigation Menu', function () {
 	it('changes active link depending on route', function() {
+//		------------------TEST 1---------------------
 		browser.get('/');
 		var activeListItem = element(by.css('.active'));
 		var text = activeListItem.findElement(by.tagName('a')).getText();
 		
 		expect(text).toEqual('Home');
+		
+//		------------------TEST 2---------------------
+		var edgesLink = element(by.linkText('Edges'));
+		edgesLink.click();
+		activeListItem = element(by.css('.active'));
+		text = activeListItem.findElement(by.tagName('a')).getText();
+		expect(text).toEqual('Edges');
 	});
 });
