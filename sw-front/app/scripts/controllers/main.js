9670dc96382949987dcaeeaee8655b9e412a2761
Nick Bonnet <nbonnet@follett.com>
2014-04-11 09:39:34 -0500

    initial commit front end

---
+++ sw-front/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('swFrontApp')
+  .controller('MainCtrl', function ($scope, $resource) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
