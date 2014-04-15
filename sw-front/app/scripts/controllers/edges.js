0208d608435f87564a770b5cc906abb8674c202a
Nick Bonnet <nbonnet@follett.com>
2014-04-15 08:47:20 -0500

    Lesson 2.5 (4:26)

--- sw-front/app/scripts/controllers/edges.js
+++ sw-front/app/scripts/controllers/edges.js
@@ -1,17 +1,22 @@
 'use strict';
 
 angular.module('swFrontApp')
-.controller('EdgesController', function ($scope, edges) {
+.controller('EdgesController', function ($scope, edges, categories) {
 	var selectedEdge = null;
 	$scope.edges = edges.query();
+	$scope.categories = categories.query();
+	
+	$scope.filterBy = {
+		search: ''
+	}
 	
 	$scope.selectEdge = function(edge) {
 		selectedEdge = (selectedEdge === edge) ? null : edge;
-	}
+	};
 	
 	$scope.isSelected = function(edge) {
 		return edge === selectedEdge;
-	}
+	};
 	
 	$scope.displayRequirements = function(reqs) {
 		var result= '';
---
+++ sw-front/app/scripts/services/categories.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('swFrontApp')
+.service('categories', function() {
+	this.query = function() {
+		//TODO: Left off here video Lesson 2.5 (4:26)
+	};
+});
